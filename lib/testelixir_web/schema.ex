39ecdeaa1c504084eb3bfbdece808005791015b0
Sebastien <sebastien.pingal@gmail.com>
2025-04-08 10:50:06 +0200

    refactor(schema): update import syntax and adjust user creation arguments

--- lib/testelixir_web/schema.ex
+++ lib/testelixir_web/schema.ex
@@ -1,25 +1,26 @@
 defmodule TestelixirWeb.Schema do
   use Absinthe.Schema
 
-  import_types TestelixirWeb.Schema.Types.UserType
+  import_types(TestelixirWeb.Schema.Types.UserType)
 
   query do
     field :users, list_of(:user) do
-      resolve (fn _, _ ->
-        {:ok, [
-          %{id: 1, name: "Bob Benco", email: "bob@benco.com"},
-          %{id: 2, name: "Ricky Framboise", email: "ricky@framboise.com"}
-        ]}
+      resolve(fn _, _ ->
+        {:ok,
+         [
+           %{id: 1, name: "Bob Benco", email: "bob@benco.com"},
+           %{id: 2, name: "Ricky Framboise", email: "ricky@framboise.com"}
+         ]}
       end)
     end
   end
 
   mutation do
     field :create_user, :user do
-      arg(name, non_null(:string))
-      arg(email,non_null(:string))
+      arg :name, :string
+      arg :email, :string
 
-      resolve(fn _, args, _, ->
+      resolve(fn _, args, _ ->
         {:ok, %{id: "3", name: args.name, email: args.email}}
       end)
     end
